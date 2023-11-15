7a80abd27f86173200bca358fdafe48254b10820
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-11-15 13:20:07 -0700

    test

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({
 
 app.get("/", function(req, res) {
 
-  const query = `SELECT table_name FROM information_schema.tables;`;
+  const query = `SELECT * FROM courses_summary`;
   
   client.query(query, (err, res) => {
         if (err) {
