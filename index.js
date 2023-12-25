5ca0f7c0dc2ef7894caa568acbb5575d6ee65fa2
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-12-25 13:07:11 -0700

    added json parser

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(bodyParser.json()); //Handles JSON requests
 
 app.post("/executeQuery", function(req, res) {
 
